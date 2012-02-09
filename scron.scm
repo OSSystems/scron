4ebfc761a4990f02ec85f4199e604e21a9fb057d
Mario Domenech Goulart <mario.goulart@gmail.com>
2012-02-09 18:25:09 -0200

    Importing

---
+++ scron.scm
@@ -0,0 +1,30 @@
+(use srfi-1 srfi-18 posix)
+
+(define (schedule! basedir)
+  (let ((wait-for-thread #f))
+    (for-each
+     (lambda (dir)
+       (let ((interval (string->number (pathname-strip-directory dir))))
+         (when interval
+           (set! wait-for-thread
+                 (thread-start!
+                  (lambda ()
+                    (let loop ()
+                      (let ((files (append (glob (make-pathname dir "*.scm"))
+                                           (glob (make-pathname dir "*.so")))))
+                        (for-each load files))
+                      (thread-sleep! interval)
+                      (loop))))))))
+     (filter directory? (glob (make-pathname basedir "*"))))
+    (when wait-for-thread (thread-join! wait-for-thread))))
+
+(define (usage #!optional exit-code)
+  (print "Usage: " (pathname-strip-directory (program-name)) " <basedir>")
+  (when exit-code (exit exit-code)))
+
+(let ((args (command-line-arguments)))
+  (when (or (null? args)
+            (member "-h" args)
+            (member "--help" args))
+    (usage 0))
+  (schedule! (car args)))
